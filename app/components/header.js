25f44b9f248f211f61b35bd8f69c63f4ec88b022
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-04-03 10:07:26 -0300

    feat: avançando no desenvolvimento da landing page
    - Adicão da área de funcionalidades
    - Criação de título de apresentação inicial
    - Insersão de imagens de destaque
    E muito mais.

--- app/components/header.js
+++ app/components/header.js
@@ -1,17 +1,30 @@
 import styles from "../../src/Sass/header.module.sass"
 import Image from "next/image"
 import { Cormorant_Garamond } from 'next/font/google'
+import { useRef } from "react"
+import { useEffect } from "react"
 
 const cormorant_garamond = Cormorant_Garamond({ subsets: ['latin'], style: ["normal"], weight: ['300'] })
 
 export default function Header(props) {
+    const divMenu = useRef(null)
+
+    useEffect(() => {
+    
+        if (divMenu && divMenu.current) {
+            divMenu.current.style.display = 'flex';
+        }
+    }, [])
+
+
+
     return (
         <div className={styles.div}>
             <div className={styles.div__logo}>
                 <Image src="/img/logo.png" width={70} height={80} alt="logo do site" />
                 <h1 className={` ${cormorant_garamond.className} ${styles.div__h1}`}>Gedf</h1>
             </div>
-            <div className={styles.menu}>
+            <div className={styles.menu} ref={divMenu}>
                 {props.estadoUsuario ? (
                     <ul className={styles.div__ul}>
                         <li>Visão geral</li>
@@ -22,12 +35,24 @@ export default function Header(props) {
                         <li>Contato</li>
                     </ul>
                 ) : (
-                    <ul className={styles.div__ul}>
-                        <li>Sobre</li>
-                        <li>Funcionalidades</li>
-                        <li>Planos e preços</li>
-                        <li>Contato</li>
-                    </ul>
+                    <>
+                        <ul className={styles.div__ul}>
+                            <li>Sobre</li>
+                            <li>Funcionalidades</li>
+                            <li>Planos e preços</li>
+                            <li>Contato</li>
+                        </ul>
+
+                        <div className={styles.menu__usuario}>
+                            <button className={styles.usuario__login}>
+                                Login
+                            </button>
+
+                            <button className={styles.usuario__sing}>
+                                Sing up
+                            </button>
+                        </div>
+                    </>
                 )}
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Header from '../app/components/header';
 import Footer from '../app/components/footer';
+import stylesIndex from "../src/Sass/index.module.sass"
+import Image from 'next/image';
 
 function Index() {
   return (
@@ -10,8 +12,56 @@ function Index() {
         <Header />
       </header>
 
-      <main>
-        
+      <main className={stylesIndex.main}>
+        <h1 className={stylesIndex.main__h1}>Transforme o controle do seu dinheiro em liberdade e conquiste seus sonhos a partir de agora</h1>
+
+        <button className={stylesIndex.main__button}>Iniciar minha liberdade</button>
+
+        <img className={stylesIndex.main__img} src="/img/Screenshot-Gedf.png" alt="Image de apresentação" />
+
+        <div className={stylesIndex.main__funcionalidades}>
+          <h2 className={stylesIndex.funcionaliddaes__h2}>O que o Gedf por oferecer a você</h2>
+          <div className={stylesIndex.funcionalidades__contener}>
+            <div className={stylesIndex.contener__gastos}>
+              <h3>Gerenciamentod dos seus gatos</h3>
+              <p>Descubra o segredo para uma vida financeira tranquila e próspera! Com o gerenciamento dos seus gastos,
+                você pode alcançar seus objetivos financeiros e realizar seus sonhos sem preocupações.
+                Chegou a hora de tomar as rédeas da sua vida financeira e conquistar a liberdade que você merece.
+                Comece hoje mesmo a transformar seus gastos em oportunidades para um futuro melhor</p>
+            </div>
+
+            <div className={stylesIndex.contener__gastos}>
+              <h3>Gerenciamentod dos seus gatos</h3>
+              <p>Descubra o segredo para uma vida financeira tranquila e próspera! Com o gerenciamento dos seus gastos,
+                você pode alcançar seus objetivos financeiros e realizar seus sonhos sem preocupações.
+                Chegou a hora de tomar as rédeas da sua vida financeira e conquistar a liberdade que você merece.
+                Comece hoje mesmo a transformar seus gastos em oportunidades para um futuro melhor</p>
+            </div>
+
+            <div className={stylesIndex.contener__gastos}>
+              <h3>Gerenciamentod dos seus gatos</h3>
+              <p>Descubra o segredo para uma vida financeira tranquila e próspera! Com o gerenciamento dos seus gastos,
+                você pode alcançar seus objetivos financeiros e realizar seus sonhos sem preocupações.
+                Chegou a hora de tomar as rédeas da sua vida financeira e conquistar a liberdade que você merece.
+                Comece hoje mesmo a transformar seus gastos em oportunidades para um futuro melhor</p>
+            </div>
+
+            <div className={stylesIndex.contener__gastos}>
+              <h3>Gerenciamentod dos seus gatos</h3>
+              <p>Descubra o segredo para uma vida financeira tranquila e próspera! Com o gerenciamento dos seus gastos,
+                você pode alcançar seus objetivos financeiros e realizar seus sonhos sem preocupações.
+                Chegou a hora de tomar as rédeas da sua vida financeira e conquistar a liberdade que você merece.
+                Comece hoje mesmo a transformar seus gastos em oportunidades para um futuro melhor</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h4>Perguntas Frequentes</h4>
+          <div>
+            
+          </div>
+        </div>
       </main>
 
       <footer>
