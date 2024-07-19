fca28ce4eb5016ecedd06f3a96a4247d492c230f
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-07-19 10:49:50 -0300

    revert: "feat: continue creating English version by changing text from Brazilian Portuguese in footer.jsx and improving the style of footer in footer.sass"
    This reverts commit 02bda5914a9dfcea2805b424a018181f3ea2bd38.

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -9,50 +9,50 @@ const cormorant_garamond = Cormorant_Garamond({ subsets: ['latin'], style: ['nor
 export default function Footer() {
   return (
     <>
-      <div className={stylesFooter.footer__contener}>
-        <div className={stylesFooter.footer__logo}>
-          <Image src="/img/logo.png" width={70} height={80} alt="logo do site" />
-          <h1 className={` ${cormorant_garamond.className} ${stylesFooter.logo__h1}`}>Gedf</h1>
-        </div>
-
-        <div className={stylesFooter.desenvolvedor}>
-          <h3 className={stylesFooter.desenvolvedor__nome}>&copy; Developed by José Leandro</h3>
-        </div>
+    <div className={stylesFooter.footer__contener}>
+      <div className={stylesFooter.footer__logo}>
+        <Image src="/img/logo.png" width={70} height={80} alt="logo do site" />
+        <h1 className={` ${cormorant_garamond.className} ${stylesFooter.logo__h1}`}>Gedf</h1>
       </div>
+
+      <div className={stylesFooter.desenvolvedor}>
+        <h3 className={stylesFooter.desenvolvedor__nome}>&copy; Desenvolvido por José Leandro</h3>
+      </div>
+    </div>
 
       <div className={stylesFooter.footer__informacoes}>
 
         <div>
-          <h2 className={stylesFooter.footer__titulo}>About Us</h2>
+          <h2 className={stylesFooter.footer__titulo}>Sobre nós</h2>
           <ul>
-            <li className={stylesFooter.titulo__li}>What is GEDF</li>
+            <li className={stylesFooter.titulo__li}>O que é GEDF</li>
           </ul>
         </div>
 
         <div>
-          <h2 className={stylesFooter.footer__titulo}>Contact</h2>
+          <h2 className={stylesFooter.footer__titulo}>Contato</h2>
           <ul>
-            <li className={stylesFooter.titulo__li}>By Phone</li>
-            <li className={stylesFooter.titulo__li}>By email</li>
+            <li className={stylesFooter.titulo__li}>Por Telefone</li>
+            <li className={stylesFooter.titulo__li}>Por email</li>
             <li className={stylesFooter.titulo__li}>Whatsapp</li>
-            <li className={stylesFooter.titulo__li}>And More</li>
+            <li className={stylesFooter.titulo__li}>E Mais</li>
           </ul>
         </div>
 
         <div>
-          <h2 className={stylesFooter.footer__titulo}>Terms and policies</h2>
+          <h2 className={stylesFooter.footer__titulo}>Termos e políticas</h2>
           <ul>
 
-            <li className={stylesFooter.titulo__li}>Terms of use</li>
-            <li className={stylesFooter.titulo__li}>Privacy Policy</li>
-            <li className={stylesFooter.titulo__li}>Cookie Policy</li>
+            <li className={stylesFooter.titulo__li}>Termos de uso</li>
+            <li className={stylesFooter.titulo__li}>Polítca de privadicade</li>
+            <li className={stylesFooter.titulo__li}>Política de cookies</li>
           </ul>
         </div>
 
         <div>
-          <h2 className={stylesFooter.footer__titulo}>Copyright</h2>
+          <h2 className={stylesFooter.footer__titulo}>Direitos autorais</h2>
           <ul>
-            <li className={stylesFooter.titulo__imagens}>Use of images and icons</li>
+            <li>Uso de imagens e icones</li>
           </ul>
         </div>
       </div>
