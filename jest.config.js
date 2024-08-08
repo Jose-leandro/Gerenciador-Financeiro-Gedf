2000aed83852a495f5d35ff1020cf1244c8d252a
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-08-08 12:23:12 -0300

    fix: correct Jest configuration error and rename `sass-transformer.js` to `babel-transformer.js`
    - Fixed an error in Jest configuration.
    - Renamed `sass-transformer.js` to `babel-transformer.js` to better with the file's intended purpose.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['C:/Users/leand/gerenciador-financeiro-gedf/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|sass|scss)$': 'identity-obj-proxy'
   },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.sass$': '<rootDir>sass-transformer.js'
-  }
+    '^.+\\.sass$': 'jest-transform-sass'
+  }  
 }
