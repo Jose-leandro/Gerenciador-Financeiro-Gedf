94743d4e03492e36252eac4ed41f36b14db1a289
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-08-11 07:44:36 -0300

    fix(eslint): resolve ESLint issues and add security configuration
    I am learning more about making my code safer and have implemented some basic security configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
     'plugin:jsx-a11y/recommended',
-    'plugin:security/recommended',
+    'plugin:@next/next/recommended', 
+    'next', 
+    'next/core-web-vitals', 
   ],
   overrides: [
   ],
@@ -21,9 +23,35 @@ module.exports = {
     'jest',
     'jsx-a11y',
     'security',
+    'no-unsanitized',
+    'sonarjs',
+    'scanjs-rules',
+    'import',
+    'next',
   ],
   rules: {
-    "jsx-a11y/alt-text": 2,
-    "jsx-a11y/anchor-has-content": 2
+    'jsx-a11y/alt-text': 2,
+    'jsx-a11y/anchor-has-content': 2,
+    'security/detect-object-injection': 'warn',
+    'no-unsanitized/method': 'error',
+    'no-unsanitized/property': 'error',
+    'sonarjs/no-duplicate-string': 'warn',
+    'scanjs-rules/no-assign-to-global': 'warn',
+    'scanjs-rules/no-implied-eval': 'error',
+    'scanjs-rules/no-unsanitized-input': 'error',
+    'import/no-cycle': 'warn', // Temporarily warn on import cycles
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      alias: {
+        map: [
+          ['@', './src', './app'], 
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
   },
 };
