13b2aebb2da395924eb17d3533457c40de0edd9f
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-09-09 08:56:04 -0300

    feat: creation, develop, and adapt dynamic menu for mobile devices

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { Cormorant_Garamond } from 'next/font/google';
 import styles from '../../src/Sass/header.module.sass';
+import useAutoRespon from '../../lib/useAutoRespon';
 
 const cormorant_garamond = Cormorant_Garamond({ subsets: ['latin'], style: ['normal'], weight: ['300'] });
 
@@ -14,6 +15,20 @@ export default function Header(props) {
     }
   }, []);
 
+console.log(useAutoRespon("600px"))
+  const renderResponsive = (containerStyle) => (
+
+
+    <div className={styles.menu} ref={divMenu}>
+    <h1 className={styles.div__li}>Overview</h1>
+    <h2 className={styles.div__li}>Your performance</h2>
+    <h3 className={styles.div__li}>Learn how to use</h3>
+    <h4 className={styles.div__li}>Settings</h4>
+    <h5 className={styles.div__li}>About</h5>
+    <h6 className={styles.div__li}>Contact</h6>
+  </div>
+  );
+
   return (
     <div className={styles.div}>
       <div className={styles.div__logo}>
@@ -21,14 +36,7 @@ export default function Header(props) {
         <h1 className={` ${cormorant_garamond.className} ${styles.div__h1}`}>Gedf</h1>
       </div>
         {props.estadoUsuario ? (
-          <div className={styles.menu} ref={divMenu}>
-            <h1 className={styles.div__li}>Overview</h1>
-            <h2 className={styles.div__li}>Your performance</h2>
-            <h3 className={styles.div__li}>Learn how to use</h3>
-            <h4 className={styles.div__li}>Settings</h4>
-            <h5 className={styles.div__li}>About</h5>
-            <h6 className={styles.div__li}>Contact</h6>
-          </div>
+        <h1>fdt</h1>
         ) : (
           <>
           <div className={styles.menu} ref={divMenu}>
